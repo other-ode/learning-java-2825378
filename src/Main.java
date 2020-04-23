df134adbd922e2318ed8e80089b61a3950bcf7c7
jmx4real <odebade.digit@gmail.com>
2020-04-23 17:43:33 +0100

    Solution to the first challenge

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Fortune teller
-        int inputtedNumber;
-
-        System.out.println("Pick a number between 1 and 10 ");
-
         Scanner input = new Scanner(System.in);
-        inputtedNumber = input.nextInt();
 
-        if (inputtedNumber < 5){
-            System.out.println("Enjoy the good fortune a friend brings you");
-        }else {
-            System.out.println("Your shoe selection will make you happy today");
+        String question = "What is the capital of Ghana?";
+        String choiceOne = "Freetown";
+        String choiceTwo = "London";
+        String choiceThree = "Accra";
+
+        //String correctAnswer = choiceThree;
+
+        System.out.println(question);
+        System.out.println("a. " + choiceOne + "\n" + "b. " + choiceTwo + "\n" + "c. " + choiceThree);
+
+        String userAnswer = input.next();
+
+        if (userAnswer.equals("c")){
+            System.out.println("Correct answer!");
+        }else{
+            System.out.println("Wrong answer! The correct answer is " + choiceThree);
         }
-
-        //While loop
-
-        Scanner secondInput = new Scanner(System.in);
-        boolean isOnRepeat = true;
-
-        while (isOnRepeat){
-            System.out.println("Playing current song!");
-            System.out.println("Do you want to move to next song? Answer yes or no");
-            String userInput = input.next();
-
-            if (userInput.equals("yes")){
-                isOnRepeat = false;
-            }
-
-        }
-        System.out.println("Playing next song");
     }
 }
