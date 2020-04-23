ae1ab7791075ed5e90154c795037ec821b50731a
jmx4real <odebade.digit@gmail.com>
2020-04-23 16:20:10 +0100

    Adding primitive and reference types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World from the other side");
+        int studentAge = 10;
+        double studentGPA = 3.43;
+
+        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Kee";
+        String studentLastName = "Woon";
+
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
     }
 }
